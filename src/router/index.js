dd86d533d32c720d113a59527d24f4358ea3c326
unknown <cm2001218@gmail.com>
2020-09-26 09:42:16 +1200

    v1.1

--- src/router/index.js
+++ src/router/index.js
@@ -17,8 +17,9 @@ const routes = [
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
+  // mode: "history",
+  // base: process.env.BASE_URL,
+  base: "miaomiao",
   routes,
 });
 
